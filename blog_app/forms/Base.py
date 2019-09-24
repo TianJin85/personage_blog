8ac54562f493e3c5a940160aff34710b0cb0a588
yxytj <307440205@qq.com>
2019-09-24 17:18:38 +0800

    增加表单

---
+++ blog_app/forms/Base.py
@@ -0,0 +1,21 @@
+from django import forms
+
+
+class ArticleForm(forms.Form):
+
+    title = forms.CharField()
+    label = forms.CharField()
+    put_date = forms.DateField()
+    title_class = forms.CharField()
+    text_type = forms.CharField()
+    blog_type = forms.CharField()
+    text_content = forms.CharField()
+    click_num = forms.CharField()
+
+
+class CommentForm(forms.Form):
+
+    Ip = forms.CharField()
+    cet_conte = forms.CharField()
+    cet_date = forms.CharField()
+
